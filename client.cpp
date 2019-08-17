1c9ced8f702738709f0e0552f055973b42499800
J. Junquera <javier@junquera.xyz>
2019-08-17 15:47:23 +0200

    Parametric client generation

--- client.cpp
+++ client.cpp
@@ -1,44 +1,128 @@
 #include "client.h"
 
-SClient::SClient(){
+void savePublicKeyToFile(PublicKey key, string filename){
+  ofstream pub;
+  pub.open(filename, ios::binary);
+  key.save(pub);
+};
 
-  EncryptionParameters parms(scheme_type::CKKS);
+PublicKey loadPublicKeyFromFile(EncryptionParameters* parms, string filename){
 
-  size_t poly_modulus_degree = 8192;
-  parms.set_poly_modulus_degree(poly_modulus_degree);
-  parms.set_coeff_modulus(CoeffModulus::Create(
-      poly_modulus_degree, { 60, 40, 40, 60 }));
+  ifstream pub;
+  pub.open(filename, ios::binary);
+  PublicKey key;
 
+  auto context = SEALContext::Create(*parms);
+  key.load(context, pub);
+
+  return key;
+};
+
+bool exists_file (string name) {
+    if (FILE *file = fopen(name.c_str(), "r")) {
+      fclose(file);
+      return true;
+    } else {
+      return false;
+    }
+};
+
+void SClient::genKeys() {
+	// generaClaves(key);
+};
+
+void SClient::setKeysFromFile(string keyFileName){
+	// loadSecretKeyFromFile(keyFileName, key);
+};
+
+void loadParametersFromFile(EncryptionParameters* parms, string filename){
   // Save parameters
-  ofstream parameters;
-  parameters.open("parameters.data", ios::binary);
-  EncryptionParameters::Save(parms, parameters);
+  ifstream parameters;
+  parameters.open(filename, ios::binary);
+  EncryptionParameters _parms = EncryptionParameters::Load(parameters);
+  parms = &_parms;
+}
 
-  auto context = SEALContext::Create(parms);
+SClient::SClient(){
+  SClient("");
+};
+
+SClient::SClient(string config_mask) {
+
+  stringstream stringStream;
+
+  stringStream.str("");
+  stringStream << config_mask << "params.data";
+  string params_path = stringStream.str();
+
+  stringStream.str("");
+  stringStream << config_mask << "public.key";
+  string pub_key_path = stringStream.str();
+
+  stringStream.str("");
+  stringStream << config_mask << "secret.key";
+  string sec_key_path = stringStream.str();
+
+  stringStream.str("");
+  stringStream << config_mask << "relin.key";
+  string rel_key_path = stringStream.str();
+
+  if(exists_file(params_path)){
+    loadParametersFromFile(parms, params_path);
+  } else {
+    parms = new EncryptionParameters(scheme_type::CKKS);
+    size_t poly_modulus_degree = 8192;
+    parms->set_poly_modulus_degree(poly_modulus_degree);
+    parms->set_coeff_modulus(CoeffModulus::Create(
+    poly_modulus_degree, { 60, 40, 40, 60 }));
+
+    // Save parameters
+    ofstream parameters;
+    parameters.open(params_path, ios::binary);
+    EncryptionParameters::Save(*parms, parameters);
+  }
+
+
+  auto context = SEALContext::Create(*parms);
   // print_parameters(context);
   // cout << endl;
 
-  KeyGenerator keygen(context);
-  public_key = keygen.public_key();
-  secret_key = keygen.secret_key();
-  relin_keys = keygen.relin_keys();
+  if( exists_file(pub_key_path) &&
+      exists_file(sec_key_path) &&
+      exists_file(rel_key_path)) {
 
-  // Save keys
-  ofstream pub, sec, rel;
-  pub.open("public.key", ios::binary);
-  sec.open("secret.key", ios::binary);
-  rel.open("relin.key", ios::binary);
+    ifstream pub, sec, rel;
+    pub.open(pub_key_path, ios::binary);
+    sec.open(sec_key_path, ios::binary);
+    rel.open(rel_key_path, ios::binary);
 
-  public_key.save(pub);
-  secret_key.save(sec);
-  relin_keys.save(rel);
+    public_key.load(context, pub);
+    secret_key.load(context, sec);
+    relin_keys.load(context, rel);
+
+  } else {
+    KeyGenerator keygen(context);
+    public_key = keygen.public_key();
+    secret_key = keygen.secret_key();
+    relin_keys = keygen.relin_keys();
+
+    // Save keys
+    ofstream pub, sec, rel;
+    pub.open(pub_key_path, ios::binary);
+    sec.open(sec_key_path, ios::binary);
+    rel.open(rel_key_path, ios::binary);
+
+    public_key.save(pub);
+    secret_key.save(sec);
+    relin_keys.save(rel);
+  }
 
   encryptor = new Encryptor(context, public_key);
   evaluator = new Evaluator(context);
   decryptor = new Decryptor(context, secret_key);
   encoder = new CKKSEncoder(context);
 
-}
+};
 
 Ciphertext SClient::encrypt(double a) {
 
@@ -116,7 +200,26 @@ vector<Distance> distance(SClient client, double mes, double temperatura){
 
 }
 
-int main() {
+void saveCiphertext(Ciphertext encrypted, string filename){
+  ofstream ct;
+  ct.open(filename, ios::binary);
+  encrypted.save(ct);
+};
+
+
+Ciphertext loadCiphertext(EncryptionParameters* parms, string filename){
+  auto context = SEALContext::Create(*parms);
+
+  ifstream ct;
+  ct.open(filename, ios::binary);
+  Ciphertext result;
+  result.load(context, ct);
+
+  return result;
+
+};
+
+int main(int argc, char** argv) {
 
   /*
   Temperatura en Junio de 2018 en Cabo de Gata
@@ -125,9 +228,15 @@ int main() {
   float y = 23.0;
 
   SClient client;
+
   vector<Distance> result = distance(client, x, y);
   for(int i = 0; i < result.size(); i++)
     cout << result[i].name << ": " << result[i].distance << endl;
+
+  Ciphertext test_x = client.encrypt(x);
+  saveCiphertext(test_x, "x.data");
+  Ciphertext test_x2 = loadCiphertext(client.parms, "x.data");
+  cout << client.decrypt(test_x2)[1] << endl;
 
 
   return 0;
--- client.h
+++ client.h
@@ -29,11 +29,15 @@ class Distance {
 class SClient {
   public:
     SClient();
+    SClient(string config_mask);
     Ciphertext encrypt(double a);
     Ciphertext encrypt(vector<double> a);
     vector<double> decrypt(Ciphertext a);
+    void genKeys();
+    void setKeysFromFile(string keyFileName);
     PublicKey public_key;
     RelinKeys relin_keys;
+    EncryptionParameters* parms;
   private:
     SecretKey secret_key;
     Encryptor* encryptor;
